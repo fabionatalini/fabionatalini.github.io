95d7a23e4a8b4be608ebf7a909edd9ef202fd897
fabio <fabionatalini@gmail.com>
2022-03-20 20:05:25 +0100

    Initial commit

---
+++ comandos_github_website.sh
@@ -0,0 +1,16 @@
+
+cd /home/fabio/Documents/automotive_purchase_orders/sitio_web
+
+# to set up, run this only the first time
+git clone https://github.com/fabionatalini/fabionatalini.github.io
+
+cd fabionatalini.github.io 
+# copy here all your files
+
+git add --all
+
+git commit -m "Initial commit"
+
+git push origin HEAD:main
+
+# now go to https://fabionatalini.github.io
